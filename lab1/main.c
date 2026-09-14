f03558f750859172a7938ff0a86e4aec37e6932f
RegMain <kharkov.ivan.an@gmail.com>
2026-09-14 12:43:19 +0300

    Added comments in main.c

--- lab1/main.c
+++ lab1/main.c
@@ -41,9 +41,9 @@ int compute_formula(queue_t formula, int values) {
 
 void print_table(FILE *file) {
   char tmp;
-  char symbols[] = "00000000000000000000000000";
+  char symbols[] = "00000000000000000000000000"; // Is there symbol ABC...Z?
   short symbols_cnt = 0;
-  while (!feof(file)) {
+  while (!feof(file)) { // Searching for variables in formula
     fscanf(file, "%c", &tmp);
     if (isalpha(tmp)) {
       tmp = toupper(tmp);
@@ -58,6 +58,7 @@ void print_table(FILE *file) {
     printf("No variables were found\n");
     return;
   }
+  // Printing the header
   for (int i = 0; i < 26; ++i) {
     if (symbols[i] == '1') {
       printf(" %c |", i+'A');
@@ -65,6 +66,9 @@ void print_table(FILE *file) {
   }
   printf(" Result\n");
   queue_t formula = validate_formula(file);
+  // Print the table itself
+  // Bit magic for getting some bit from our string (000, 001, 010, ...,
+  // 1 << symbols_cnt - 1 (amount of possible prompts to our formula is 2**symbols_cnt))
   for (int i = 0; i < (1 << symbols_cnt); ++i) {
     for (int j = 1; j <= symbols_cnt; ++j) {
       printf(" %d |", (i >> (symbols_cnt - j)) & 1);
